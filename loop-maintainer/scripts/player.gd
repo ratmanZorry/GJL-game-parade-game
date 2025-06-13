2b9c55a0c9b6911e27383a9dca7875094a7e90af
ratmanZorry <ratmanzorry@gmail.com>
2025-06-13 12:19:26 +0300

    added tilemap and movement

--- loop-maintainer/scripts/player.gd
+++ loop-maintainer/scripts/player.gd
@@ -1,1 +1,22 @@
 extends CharacterBody2D
+
+@export var head: AnimatedSprite2D
+@export var body: AnimatedSprite2D
+@export var arms: AnimatedSprite2D
+@export var legs: AnimatedSprite2D
+
+@export var gravity = 300.0
+
+@export var speed := 100.0
+
+func _physics_process(delta: float) -> void:
+	if !is_on_floor():
+		velocity.y += gravity
+	
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+	
+	move_and_slide()
