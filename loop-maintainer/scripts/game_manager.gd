16ad418f055393b02dbc460366f0e84c466b33d5
ratmanZorry <ratmanzorry@gmail.com>
2025-06-14 18:34:21 +0300

    added dialogue box and stuff

--- loop-maintainer/scripts/game_manager.gd
+++ loop-maintainer/scripts/game_manager.gd
@@ -3,5 +3,6 @@ extends Node
 var should_spawn_obstacles := false
 
 func _ready() -> void:
-	await get_tree().create_timer(3).timeout
-	should_spawn_obstacles = true
+	#await get_tree().create_timer(3).timeout
+	#should_spawn_obstacles = true
+	pass
