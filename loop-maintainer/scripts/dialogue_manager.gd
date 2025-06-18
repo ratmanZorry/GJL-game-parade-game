4b4b1d78937a9cf7b9dcbd427669487e9b4473eb
ratmanZorry <ratmanzorry@gmail.com>
2025-06-18 19:46:43 +0300

    AASSD

--- loop-maintainer/scripts/dialogue_manager.gd
+++ loop-maintainer/scripts/dialogue_manager.gd
@@ -5,21 +5,16 @@ signal dialogue_end
 
 @export var pre_dialogue_wait_time: float
 @export var pre_WIZARD_dialogue_wait_time: float = 0.275
-@export var start_dialogue: Array[loop_dialogue_number]
 
-@export_category("wizard dialogues")
-@export var wizard_dialogue_1: Array[loop_dialogue_number]
-@export var wizard_dialogue_2: Array[loop_dialogue_number]
-@export var wizard_dialogue_3: Array[loop_dialogue_number]
-@export var wizard_dialogue_4: Array[loop_dialogue_number]
-@export var wizard_dialogue_5: Array[loop_dialogue_number]
+@export var start_dialogue: Array[loop_dialogue_number]
+@export var wizard_dialogue: Array[loop_dialogue_number]
 
 @export var dialogue_area: Area2D
-
 @onready var dialogue_box: Node2D = get_tree().get_first_node_in_group("dialogue_box")
 
 
 func _ready() -> void:
+	print(get_tree().current_scene.name)
 	await get_tree().create_timer(pre_dialogue_wait_time).timeout
 
 	for block in start_dialogue:
@@ -44,13 +39,13 @@ func wait_for_dialogue_key() -> void:
 				else:
 					break
 
-
 func _on_dialogue_area_body_entered(body: Node2D) -> void:
 	if dialogue_area and dialogue_area.is_in_group("wizard_dialogue_area"):
 		if GameManager.can_meet_wizard:
 			await get_tree().create_timer(pre_WIZARD_dialogue_wait_time).timeout
 			GameManager.can_meet_wizard = false
-			for block in wizard_dialogue_1:
+
+			for block in wizard_dialogue:
 				if block.loop_number == GameManager.loop_number:
 					for line in block.lines:
 						GameManager.is_in_dialogue = true
