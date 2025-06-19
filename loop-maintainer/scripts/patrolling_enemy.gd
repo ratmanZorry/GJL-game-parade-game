69df6192c17bf3988707f6bfe9f9c37e0a2fa350
ratmanZorry <ratmanzorry@gmail.com>
2025-06-19 17:38:41 +0300

    asdsadsa

--- loop-maintainer/scripts/patrolling_enemy.gd
+++ loop-maintainer/scripts/patrolling_enemy.gd
@@ -7,7 +7,9 @@ var is_dead
 var isGoingRight = false
 
 @export var anim: AnimatedSprite2D
+@export var animator: AnimationPlayer
 @export var kill_area: Area2D
+@export var queue_free_timer: Timer
 
 func _physics_process(delta: float):
 	if is_dead:
@@ -30,9 +32,15 @@ func _physics_process(delta: float):
 
 
 func _on_death_collider_area_entered(area: Area2D) -> void:
-	if can_die and area.is_in_group("player_feet"):
+	if can_die and area.is_in_group("player_feet") and not is_dead:
 		is_dead = true
 		anim.play("dead")
 		collision_layer = 0
 		collision_mask = 0
 		kill_area.queue_free()
+		queue_free_timer.start()
+		animator.play("fade")
+
+
+func _on_queue_free_timer_timeout() -> void:
+	queue_free()
