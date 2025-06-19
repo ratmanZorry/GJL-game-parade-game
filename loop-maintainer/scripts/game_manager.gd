f8a71caf8238968877bd5746831dd09809be1df6
ratmanZorry <ratmanzorry@gmail.com>
2025-06-19 18:20:25 +0300

    asf adf

--- loop-maintainer/scripts/game_manager.gd
+++ loop-maintainer/scripts/game_manager.gd
@@ -5,11 +5,11 @@ var did_game_start := false
 var is_in_dialogue := false
 var can_meet_wizard := false
 
+var spike_data: Array = []
+var patrol_enemy_data: Array = []
 var next_player_spawn_position: Vector2 = Vector2.INF
 
 var loop_number = 0
 
 func _ready() -> void:
-		#await get_tree().create_timer(3).timeout
-		#should_spawn_obstacles = true
 	pass
--- loop-maintainer/scripts/obstacle_spawner.gd
+++ loop-maintainer/scripts/obstacle_spawner.gd
@@ -2,13 +2,39 @@ extends Node
 
 var did_spawn_obstacles := false
 
-@export var easy_obstacles: Array[PackedScene]
+@export var spike_obstacles: Array[PackedScene]
+@export var patroll_enemy_obstacles: Array[PackedScene]
+
+func _ready():
+	for data in GameManager.spike_data:
+		var index = data["scene_index"]
+		var pos = data["position"]
+		var scene = spike_obstacles[index]
+		var inst = scene.instantiate()
+		inst.global_position = pos
+		add_child(inst)
+
+	for data in GameManager.patrol_enemy_data:
+		var index = data["scene_index"]
+		var pos = data["position"]
+		var scene = patroll_enemy_obstacles[index]
+		var inst = scene.instantiate()
+		inst.global_position = pos
+		add_child(inst)
 
 func _process(delta: float) -> void:
 	if GameManager.should_spawn_obstacles and not did_spawn_obstacles:
 		for item in JumpLocationManager.jump_locations:
-			var obstacle_scene = easy_obstacles.pick_random()
+			var spike_index = randi() % spike_obstacles.size()
+			var obstacle_scene = spike_obstacles[spike_index]
 			var obstacle_instance = obstacle_scene.instantiate()
 			obstacle_instance.global_position = item
 			add_child(obstacle_instance)
-			GameManager.should_spawn_obstacles = false
+
+			GameManager.spike_data.append({
+				"position": item,
+				"scene_index": spike_index
+			})
+
+		GameManager.should_spawn_obstacles = false
+		did_spawn_obstacles = true
