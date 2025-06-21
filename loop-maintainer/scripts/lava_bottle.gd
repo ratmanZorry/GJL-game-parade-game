1783e52a97b14c38e4539044f6980fcb91ef9c9a
ratmanZorry <ratmanzorry@gmail.com>
2025-06-21 20:50:05 +0300

    jnsdfjnmsgdfmjn

--- loop-maintainer/scripts/lava_bottle.gd
+++ loop-maintainer/scripts/lava_bottle.gd
@@ -3,12 +3,14 @@ extends Node2D
 @export var fountain_lasting_timer: Timer
 @export var fountain_timer: Timer
 
+@export var bottle: AnimatedSprite2D
 @export var fountain: AnimatedSprite2D
 @export var kill_area: Area2D
 
 
 func _on_fountain_timer_timeout() -> void:
 	fountain.visible = true
+	bottle.visible = true
 	kill_area.monitorable = true
 	kill_area.monitoring = true
 	fountain_lasting_timer.start()
